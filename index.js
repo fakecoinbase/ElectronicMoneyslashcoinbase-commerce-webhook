aaa8ca88c2bd7289c74708cb65063a39ee3d8a6e
Emeka Augustine <cafafanscoders@gmail.com>
2020-04-12 14:21:40 +0800

    Add environment variable file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 const port = 3000
 
+//Coinbase Commerce Seceret from environment varible
+COINBASE_COMMERCE_SECRET = process.env.COINBASE_COMMERCE_SECRET
+
 app.get('/', (req, res) => res.send('Webhook App!'))
 
-app.listen(port, () => console.log(`Webhook app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Webhook app listening at http://localhost:${port} with secrete code of ${COINBASE_COMMERCE_SECRET}`))
